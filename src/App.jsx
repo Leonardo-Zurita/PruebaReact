7ba19af53da68ecaf74568835b1bc4ee9492b21e
Leonardo Zurita <leonardodanielzurita@gmail.com>
2022-10-31 18:23:47 -0300

    primera app con React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+     <h1 id='Titulo'>Hello world</h1>
+     <p id='descripcion'>Esta es mi primera pagina con React</p>
+    </>
+  );
+}
+
+export default App;
